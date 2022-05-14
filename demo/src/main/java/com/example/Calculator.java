eac8dddfce259cf3dd936d691ba54ab8393a42b4
sly503 <fatjonrami@gmail.com>
2022-05-14 12:01:21 +0200

    added some methods and tests

--- demo/src/main/java/com/example/Calculator.java
+++ demo/src/main/java/com/example/Calculator.java
@@ -5,5 +5,52 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mod(int a, int b) {
+        return a % b;
+    }
+
+    public int pow(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
+    public int fact(int a) {
+        int result = 1;
+        for (int i = 1; i <= a; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public int fib(int a) {
+        int result = 0;
+        int prev = 0;
+        int next = 1;
+        for (int i = 0; i < a; i++) {
+            result = prev + next;
+            prev = next;
+            next = result;
+        }
+        return result;
+    }
+
+
     
+
 }
--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -28,6 +28,107 @@ public class AppTest
         assertFalse( calc.sum(1, 2) == 4 );
     }
 
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.sub(1, 2) == -1 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse2()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.sub(1, 2) == 0 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.mul(1, 2) == 2 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse3()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.mul(1, 2) == 3 );
+    }
+
+
+    @Test
+    public void shouldAnswerWithTrue4()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.div(1, 2) == 0 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse4()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.div(1, 2) == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue5()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.mod(1, 2) == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse5()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.mod(1, 2) == 0 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue6()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.pow(1, 2) == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse6()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.pow(1, 2) == 0 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue7()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.fact(1) == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse7()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.fact(1) == 0 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue8()
+    {
+        Calculator calc = new Calculator();
+        assertTrue( calc.fib(1) == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse8()
+    {
+        Calculator calc = new Calculator();
+        assertFalse( calc.fib(1) == 0 );
+    }
+
+
+
 
 
 }
